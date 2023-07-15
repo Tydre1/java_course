689d17b8dfa82832954267a542f5a51f60e478fb
andre_barbosa <oliveiraandre945@gmail.com>
2023-07-15 20:50:02 -0300

    Java projects that I made watching Alura courses

---
+++ bytebox-encapsulado/src/TestaConta.java
@@ -0,0 +1,10 @@
+
+public class TestaConta {
+	
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta(1337, 23334);
+		conta.deposito(200.0);
+		
+	}
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaValores.java
@@ -0,0 +1,14 @@
+
+public class TestaValores {
+
+	public static void main(String[] args) {
+		
+		int primeiroNumero = 5;
+		int segundoNumero = 2;
+		segundoNumero = primeiroNumero;
+		primeiroNumero = 10;
+		
+		// quanto vale o segundoNumero?
+		System.out.println(segundoNumero);
+	}
+}
